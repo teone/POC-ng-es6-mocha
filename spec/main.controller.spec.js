2fc9fe7259f6d7bc204fdf6ec91e385dd019bb47
Matteo Scandolo <matteo.scandolo@link-me.it>
2015-09-13 12:00:49 +0200

    First real test with mocked service

--- spec/main.controller.spec.js
+++ spec/main.controller.spec.js
@@ -2,8 +2,24 @@
 
 import {expect} from 'chai';
 
+import {MainController} from '../src/app/controllers/main.controller.js';
+
+var mockService, ctrl;
+
 describe('The Main Controller', function() {
-  it('should pass', function(){
-    expect(true).to.equal(true);
-  })
+
+  beforeEach(function() {
+
+    // define the mocked service
+    mockService = {
+      list: [1, 2]
+    }
+
+    // instantiate the controller
+    ctrl = new MainController(mockService);
+  });
+
+  it('should load the list', function(){
+    expect(ctrl.list.length).to.equal(2);
+  });
 });
