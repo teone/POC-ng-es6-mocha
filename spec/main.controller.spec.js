d1e77051f4d500101a33c322fa57c8ff6cd4d54f
Matteo Scandolo <matteo.scandolo@link-me.it>
2015-09-13 12:12:52 +0200

    Spying on service function

--- spec/main.controller.spec.js
+++ spec/main.controller.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import {expect} from 'chai';
+const sinon = require('sinon'); // understand why import does not work here
 
 import {MainController} from '../src/app/controllers/main.controller.js';
 
@@ -12,7 +13,8 @@ describe('The Main Controller', function() {
 
     // define the mocked service
     mockService = {
-      list: [1, 2]
+      list: [1, 2],
+      add: sinon.spy()
     }
 
     // instantiate the controller
@@ -21,5 +23,10 @@ describe('The Main Controller', function() {
 
   it('should load the list', function(){
     expect(ctrl.list.length).to.equal(2);
+  });
+
+  it('should add an item to the service', function(){
+    ctrl.addToService(3);
+    expect(mockService.add.called).to.equal(true);
   });
 });
